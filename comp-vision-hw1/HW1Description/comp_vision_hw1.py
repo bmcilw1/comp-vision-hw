fe1df2785243277fd0996d6ca1c1a084b298baa6
Brian McIlwain <brianwmcilwain@gmail.com>
2017-01-30 20:53:55 -0600

    Added prob1 part 1

--- comp-vision-hw1/HW1Description/comp_vision_hw1.py
+++ comp-vision-hw1/HW1Description/comp_vision_hw1.py
@@ -3,14 +3,25 @@
 # Homework 1
 
 import cv2
+from matplotlib import pyplot as plt
 
 # Problem 1
 def myHEQ_RGB(img):
-    cv2.imshow('HEQ_RGB', img)
+    bImg,gImg,rImg = cv2.split(img)
+
+    bEqu = cv2.equalizeHist(bImg)
+    gEqu = cv2.equalizeHist(gImg)
+    rEqu = cv2.equalizeHist(rImg)
+
+    newImg = cv2.merge([bEqu, gEqu, rEqu])
+
+    cv2.imshow('HEQ_RGB', newImg)
     cv2.waitKey(0)
     return
 
 img = cv2.imread('HW1Description/TestImages/Castle_badexposure.jpg')
+cv2.imshow('DEFAULT', img)
+
 myHEQ_RGB(img)
 
 cv2.destroyAllWindows()
