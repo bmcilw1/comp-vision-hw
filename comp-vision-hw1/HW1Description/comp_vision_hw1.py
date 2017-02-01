fb2eb4f4eb00f60db3ffddb16088bcdc76148fec
Brian McIlwain <brianwmcilwain@gmail.com>
2017-02-01 12:26:44 -0600

    More work on 2.3

--- comp-vision-hw1/HW1Description/comp_vision_hw1.py
+++ comp-vision-hw1/HW1Description/comp_vision_hw1.py
@@ -3,6 +3,23 @@
 # Homework 1
 import cv2
 
+refPt = []
+SelectROI = 0
+ROI_ONLY = []
+
+def mouseFunc(event, x, y, flags, param):
+    #grab references to the global variables
+    global refPt, SelectROI    
+    if event == cv2.EVENT_LBUTTONDOWN and SelectROI == 1:
+        refPt = [(x,y)]
+        SelectROI = 2
+    elif event == cv2.EVENT_LBUTTONUP and SelectROI == 2:
+        refPt.append((x,y))
+        SelectROI = 3
+        global ROI_ONLY
+        ROI_ONLY = myHEQ_YCRCB(img[refPt[0][1]:refPt[1][1],refPt[0][0]:refPt[1][0]])
+        cv2.imshow('ROI', ROI_ONLY)
+
 # Problem 1: Write a function, called myHEQ_RGB(img), to apply histogram equalization directly on a color image.
 def myHEQ_RGB(img):
     # Part 1: Split the RGB channels into three, each of which is a grayscale image;
@@ -17,7 +34,6 @@ def myHEQ_RGB(img):
     newImg = cv2.merge([bEqu, gEqu, rEqu])
 
     # Part 4: Show the result in a window named “HEQ_RGB”
-    cv2.imshow('HEQ_RGB', newImg)
     return newImg
 
 # Problem 2: Write a function, called myHEQ_YCRCB(img), to apply histogram equalization on the Y channel of a color image.
@@ -35,30 +51,45 @@ def myHEQ_YCRCB(img):
     newImg = cv2.cvtColor(newImgYCrCb, cv2.COLOR_YCrCb2BGR)
 
     # Part 5: Show the result in a window named “HEQ_YCRCB”
-    cv2.imshow('HEQ_YCRCB', newImg)
     return newImg
 
 # Problem 2: Develop a ROI (Region of Interest) Selection Tool and apply Local Equalization:
 def myHEQ_ROI(img):
     # Part 1: Implement a mouse-event handling function to allow user to select a rectangular region of Interest (ROI) in the image.
-    newImg = cv2.cvtColor(img, cv2.COLOR_RGB2YCrCb)
+    imgName = 'ROI_SELECT'
+    cv2.imshow(imgName, img)
+    cv2.setMouseCallback(imgName, mouseFunc)
+    global SelectROI
+    SelectROI = 1
 
+    while SelectROI != 3:
+        cv2.waitKey(0)
+
+    print 'SelectROI == 3'
     # Part 2: Use “myHEQ_YCRCB()” to equalize the ROI region and get an enhanced patch
+    newImgROI = myHEQ_YCRCB(ROI_ONLY)
 
     # Part 3: Update the ROI in the image using this new enhanced patch
+    newImg = newImgROI
 
     # Part 5: Show the final result in a window named “HEQ_ROI”
-    cv2.imshow('HEQ_ROI', img)
     return newImg
 
 # Test your Step 1 and 2 using two under exposed images. And test your Step 3 program using the NikonContest2016Winner and portrait images.
-img = cv2.imread('HW1Description/TestImages/Castle_badexposure.jpg')
-cv2.imshow('DEFAULT', img)
-
 # Save your enhanced images to “HEQ_RGB.png”, “HEQ_YCRCB.png”, and “HEQ_ROI.png” respectively
-cv2.imwrite('HW1Description/HEQ_RGB.png', myHEQ_RGB(img))
-cv2.imwrite('HW1Description/HEQ_YCRCB.png', myHEQ_YCRCB(img))
-cv2.imwrite('HW1Description/HEQ_ROI.png', myHEQ_ROI(img))
+img = cv2.imread('HW1Description/TestImages/Castle_badexposure.jpg')
+#cv2.imshow('DEFAULT', img)
 
-cv2.waitKey(0)
+RGB = myHEQ_RGB(img)
+cv2.imshow('HEQ_RGB', RGB)
+cv2.imwrite('HW1Description/HEQ_RGB.png', RGB)
+
+YCRCB = myHEQ_YCRCB(img)
+cv2.imshow('HEQ_YCRCB', YCRCB)
+cv2.imwrite('HW1Description/HEQ_YCRCB.png', YCRCB)
+
+ROI = myHEQ_ROI(img)
+cv2.imshow('HEQ_ROI', ROI)
+cv2.imwrite('HW1Description/HEQ_ROI.png', ROI)
+
 cv2.destroyAllWindows()
