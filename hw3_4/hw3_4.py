373b550cc3b2898feeaec6f603d97b40c828c4ed
Brian McIlwain <brianwmcilwain@gmail.com>
2017-04-06 13:03:16 -0500

    Fixed display issue with Harris cd

--- hw3_4/hw3_4.py
+++ hw3_4/hw3_4.py
@@ -4,28 +4,30 @@
 import cv2
 import numpy as np
 
-def harris(img, blockSize, kSize, k, thresh):
+def harris(img, blockSize, kSize, k, cThresh):
     c = cv2.cornerHarris(img, blockSize, kSize, k)
-    cshow = img.copy()
+    cShow = img.copy()
 
-    # viewing Harris corner detecion
+    # viewing Harris corner detecion adapted from: 
     # http://docs.opencv.org/3.0-beta/doc/py_tutorials/py_feature2d/py_features_harris/py_features_harris.html
 
     # result is dilated for marking the corners
     cDial = cv2.dilate(c, None)
 
-    # Threshold for an optimal value
-    cshow[cDial>thresh*cDial.max()]= 255
+    # Threshold for corner detection
+    cShow[cDial>cThresh*cDial.max()]= 255
 
-    # Show harris corner images
-    cv2.imshow("Corners img", cshow)
-
-    return c
+    return c, cShow
 
 def hw3(i1, i2):
-    c1 = harris(i1, 2, 3, .04, .007)
-    c2 = harris(i2, 2, 3, .04, .007)
+    cThresh = .007
 
+    c1, c1Show = harris(i1, 2, 3, .04, cThresh)
+    c2, c2Show = harris(i2, 2, 3, .04, cThresh)
+
+    # Show harris corner images
+    cv2.imshow("Corners img1", c1Show)
+    cv2.imshow("Corners img2", c2Show)
 
     return
 
