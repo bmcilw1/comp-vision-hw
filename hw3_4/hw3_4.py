dedaf74f86139145091cda1cb8077c846ad7f940
Brian McIlwain <brianwmcilwain@gmail.com>
2017-04-19 09:19:18 -0500

    Added half of padding function

--- hw3_4/hw3_4.py
+++ hw3_4/hw3_4.py
@@ -4,10 +4,14 @@
 import cv2
 import numpy as np
 
-def harris(img, blockSize, kSize, k, cThresh):
-    # Convert to grayscale and perform harris
+def init_image(img):
+    # Convert to grayscale
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    c = cv2.cornerHarris(gray, blockSize, kSize, k)
+    return gray
+
+def harris(img, blockSize, kSize, k, cThresh):
+    # Perform harris
+    c = cv2.cornerHarris(img, blockSize, kSize, k)
     cShow = img.copy()
 
     # viewing Harris corner detecion adapted from: 
@@ -18,7 +22,7 @@ def harris(img, blockSize, kSize, k, cThresh):
     cDial = cv2.dilate(c, None)
 
     # Threshold for corner detection
-    cShow[cDial>cThresh*cDial.max()]= [0,0,255]
+    cShow[cDial>cThresh*cDial.max()]= 0
 
     return c, cShow
 
@@ -39,7 +43,7 @@ def getCornerCoordinates(c, cThresh):
 
     # NMS- Only keep local maximum pixels in each group for each corner
     kernel = np.ones((4,4),np.uint8)
-    cNMS = cv2.erode(c,kernel,iterations = 1)
+    cNMS = cv2.erode(c,kernel,iterations = 2)
 
     # Grab max points
     cmax = cNMS.max()
@@ -57,12 +61,29 @@ def getCornerCoordinates(c, cThresh):
 
     return np.asarray(cord)
 
+def padding(img, padSize):
+    img_pad = np.zeros((img.shape[1] + 2*padSize,img.shape[0] + 2*padSize), img.dtype)
+
+    print "img.shape"
+    print img.shape
+
+    x, y = img.shape
+    for x in range(0, x):
+        for y in range(0, y):
+            img_pad[y+padSize,x+padSize] = img[y,x]
+
+    return img_pad
+
 def score_ZNCC(patch1, patch2):
 
     return 0
 
 def matchKeyPts(img1, img2, patchSize, corners1, corners2, maxScoreThresh):
     match = []
+
+    img = padding(img1, 15)
+
+    cv2.imshow("padding ", img)
 
 
     return match
@@ -114,12 +135,15 @@ def draw_matches(img1, img2, corners1, corners2, matches):
 ##################
 #  main
 
-# Load images as grayscale
 img1 = cv2.imread('hw3_4/TestingImages/goldengate-02.png')
 img2 = cv2.imread('hw3_4/TestingImages/goldengate-03.png')
 
 #cv2.imshow("Original img1", img1)
 #cv2.imshow("Original img2", img2)
+
+# convert images to grayscale
+img1 = init_image(img1)
+img2 = init_image(img2)
 
 hw3(img1, img2)
 
